1b81499e294cf084a9c889601d7225595abbc419
johannes <johannes.hermann@sandstorm.de>
2024-04-09 09:03:04 +0200

    TASK: automated crontab in laravel server for schedules

--- laravel-backend/routes/console.php
+++ laravel-backend/routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Models\Troops;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+
+
+
