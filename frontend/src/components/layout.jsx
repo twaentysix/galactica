2cd903f2ef487dd1650426fd06c92fde96e237b3
fridthjofr <fridthjofr@tutanota.com>
2024-04-12 14:12:33 +0200

    12er grid as base layout

---
+++ frontend/src/components/layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Layout = ({ props }) => {
+    return (
+        <div className="container mx-auto px-4">
+            <div className="grid grid-cols-12 gap-1">
+                <div className="col-span-12 sm:col-span-12 md:col-span-3 lg:col-span-3 xl:col-span-3 2xl:col-span-3">{props.children}</div>
+                <div className="col-span-12 sm:col-span-12 md:col-span-3 lg:col-span-3 xl:col-span-3 2xl:col-span-3">{props.children}</div>
+                <div className="col-span-12 sm:col-span-12 md:col-span-3 lg:col-span-3 xl:col-span-3 2xl:col-span-3">{props.children}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
+
+
