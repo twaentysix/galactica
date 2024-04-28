5a9f06bf196777e3ed73322193a5b2be97acfe44
Sami Salih <info@twaentysix.com>
2024-04-28 17:24:17 +0200

    feat: Edited button.tsx

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -25,6 +25,23 @@ const LoginPage = () => {
 
     return (
         <div id="loginScreen" className="lg:flex">
+            <div id="dialog-pre-screen" className="absolute bg-g_dark_translucent animate-blur-in h-screen w-screen z-10 flex justify-center items-center">
+                <dialog open className="p-10 bg-white rounded-3xl shadow-2xl w-1/3 h-1/3 flex flex-col justify-between">
+                    <div id="dialog-headline-wrapper mb-5">
+                        <h4 className="text-g_dark text-3xl">Dialog Title</h4>
+                    </div>
+                    <div id="dialog-body-wrapper">
+                        <p className="text-g_dark">Dialog body</p>
+                    </div>
+                    <div id="dialog-meta-info" className="mb-5">
+                        <span>Here Icons Please</span>
+                    </div>
+                    <div id="dialog-button-area" className="flex flex-row gap-2 justify-between items-center w-full">
+                        <Button>Cancel</Button>
+                        <Button>Apply</Button>
+                    </div>
+                </dialog>
+            </div>
             <div className="hidden lg:block lg:w-2/3 h-dvh overflow-y-hidden">
                 <img
                     src={LoginImage}
@@ -41,7 +58,7 @@ const LoginPage = () => {
                         <h1>Welcome to Galactica</h1>
                         <p className="">
                             Login to your account
-                        </p>
+                        </p>    
                     </div>
                     <form onSubmit={handleSubmit}>
                         <div className="flex flex-col gap-5">
