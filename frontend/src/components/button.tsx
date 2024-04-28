707ed23eba14be4adc037309ec2b8deff63858e4
Sami Salih <info@twaentysix.com>
2024-04-28 17:23:49 +0200

    feat: Edited button

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,6 +1,6 @@
 const Button = (props: any) => {
   return(
-    <button className="rounded-md text-g_dark font-bold bg-g_light_gradient w-100 px-5 py-2 hover:brightness-150 transition-all ease-in-out duration-200">
+    <button className="rounded-md border-g_light shadow-xl text-g_dark font-bold bg-g_light_gradient w-100 px-5 py-2 hover:brightness-150 transition-all ease-in-out duration-200">
       {props.children}
     </button>
   );
