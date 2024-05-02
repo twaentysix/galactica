bcbbcf9a37550a23e707eba9127cfacde71bfd3f
Sami Salih <info@twaentysix.com>
2024-05-02 16:17:43 +0200

    fix: Made button content justified to center

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,6 +1,6 @@
 const Button = (props: any) => {
   return(
-    <button className="flex items-center gap-2 rounded-md border-g_light shadow-xl text-g_dark font-bold bg-g_light_gradient w-100 px-5 py-2 hover:brightness-150 transition-all ease-in-out duration-200">
+    <button className="flex items-center gap-2 justify-center rounded-md border-g_light shadow-xl text-g_dark font-bold bg-g_light_gradient w-100 px-5 py-2 hover:brightness-150 transition-all ease-in-out duration-200">
       {props.children}
     </button>
   );
