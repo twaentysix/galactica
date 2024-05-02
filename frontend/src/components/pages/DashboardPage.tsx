89ad9267eccf309d0589fccc5fd83a4816d32454
johannes <johannes.hermann@sandstorm.de>
2024-05-02 17:03:07 +0200

    TASK: Galaxies into Control Unit

--- frontend/src/components/pages/DashboardPage.tsx
+++ frontend/src/components/pages/DashboardPage.tsx
@@ -3,21 +3,24 @@ import Icon from "../Icon";
 import Layout from "../Layout";
 import CustomCard from "../customCard";
 import { useEffect, useState } from "react";
-import {collector, base, fleet} from "@/lib/types.ts";
+import {collector, base, fleet, galaxy} from "@/lib/types.ts";
 import {renderCollector, renderFleet} from "@/lib/RenderFunctions.tsx";
 
 const DashboardPage = () => {
 
     const [baseData, setBaseData] = useState<base[]>([])
-
+    const [galaxiesData, setGalaxiesData] = useState<galaxy[]>([])
     const [selectedBase, setSelectedBase] = useState<base>()
+    const [starMapActive, setStarMap] = useState<boolean>(false)
 
     useEffect(() => {
-        DataHandler.getBases().then(data => {setBaseData(data); setSelectedBase(data[0]);});
+        DataHandler.getBases().then(data => {setBaseData(data); data.length > 0 && setSelectedBase(data[0])});
+        DataHandler.getGalaxies().then(data => {setGalaxiesData(data)});
     }, []);
 
 
     const changeBase = (base : base) => {
+        starMapActive && setStarMap(false);
         setSelectedBase(base);
     };
 
@@ -28,10 +31,19 @@ const DashboardPage = () => {
                 {/* Top bar for the first column */}
                 <div className="bg-g_light h-10 px-4 flex items-center justify-center rounded-t-lg">
                     {/* Top bar content */}
-                    <span className="text-g_dark font-headline font-bold">Milkyway</span>
+                    <span className="text-g_dark font-headline font-bold">Control Unit</span>
                 </div>
                 {/* Content */}
                 <div className="">
+                    <CustomCard
+                        className={'mb-10 mt-5'}
+                        key={galaxiesData}
+                        backgroundColor="bg-g_planet_gradient"
+                        title={'Sternenkarte'}
+                        value={'Total Galaxies: ' + galaxiesData.length}
+                        svg={<Icon type="planet1" size="50"/>}
+                        onClick={() => setStarMap(true)}
+                    />
                     <div className="m-4">
                         <p className="text-md font-main font-bold">Bases</p>
                         <div className="flex items-center">
@@ -39,21 +51,20 @@ const DashboardPage = () => {
                             <p className="text-md font-main font-bold">{baseData.length}</p>
                         </div>
                     </div>
-                    {baseData.map((base:base) => (
-                            <CustomCard
-                                className={'mb-5'}
-                                key={base.id}
-                                backgroundColor="bg-g_planet_gradient"
-                                title={base["name"]}
-                                status={"Created at: " + new Date(base["createdAt"]).toDateString() }
-                                icon={<Icon type="medal" size="20" />}
-                                value={base["level"]}
-                                svg={<Icon type="planet1" size="50" />}
-                                onClick={() => changeBase(base)}
-                            />
-
+                    {baseData.map((base: base) => (
+                        <CustomCard
+                            className={'mb-5'}
+                            key={base.id}
+                            backgroundColor="bg-g_planet_gradient"
+                            title={base["name"]}
+                            status={"Created at: " + new Date(base["createdAt"]).toDateString()}
+                            icon={<Icon type="medal" size="20"/>}
+                            value={base["level"]}
+                            svg={<Icon type="planet1" size="50"/>}
+                            onClick={() => changeBase(base)}
+                        />
                     ))}
-                    
+
                 </div>
             </div>
             {/* Bigger column using 7 */}
@@ -78,20 +89,31 @@ const DashboardPage = () => {
                 {/* Content */}
                 <div className="p-8">
                     {/* Add content here */}
-                    <h1 className={'mb-5'}>Collectors</h1>
+                    {!starMapActive && <h1 className={'mb-5'}>Collectors</h1>}
                     <div className="grid grid-cols-3 gap-8 mb-10">
                         {
-                            baseData[0] && selectedBase &&
+                            starMapActive && galaxiesData.map((galaxy: galaxy) => (
+                                <CustomCard
+                                    className={'mb-5'}
+                                    key={galaxy.id}
+                                    backgroundColor="bg-g_planet_gradient"
+                                    title={galaxy.name}
+                                    status={"Amount of Planets: " + galaxy.planets.length}
+                                    svg={<Icon type="planet2" size="50"/>}
+                                />
+                            ))}
+                        {
+                            baseData[0] && selectedBase && !starMapActive &&
                             (selectedBase.collectors as collector[]).map((collector: collector) => (
                                 renderCollector(collector)
                             ))
                         }
                     </div>
-
-                    <h1 className={'mb-5'}>Harbour</h1>
+                    {!starMapActive&& <h1 className={'mb-5'}>Harbour</h1>}
                     <div className="grid grid-cols-3 gap-8 mb-10">
                         {
-                            baseData[0] && selectedBase &&
+
+                            baseData[0] && selectedBase && !starMapActive &&
                             (selectedBase.harbour.fleets as fleet[]).map((fleet: fleet) => (
                                 renderFleet(fleet)
                             ))
