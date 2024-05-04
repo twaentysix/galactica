6f4f270c89f320b01d167aa4c33a4a8716840320
johannes <johannes.hermann@sandstorm.de>
2024-05-04 18:58:01 +0200

    FIX: updated Seeder

--- laravel-backend/database/seeders/DatabaseSeeder.php
+++ laravel-backend/database/seeders/DatabaseSeeder.php
@@ -2,17 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Harbours;
-use App\Models\Bases;
-use App\Models\Galaxies;
-use App\Models\Planets;
-use App\Models\ResourceCollectors;
-use App\Models\Resources;
 use App\Models\Fleets;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Galaxies;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,76 +13,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::create([
-            'name' => 'Joriex',
-            'email' => 'contact@joriex.de',
-            'password' => 'password',
-            'email_verified_at' => Carbon::now(),
-        ]);
-        $user->save();
 
         $galaxy = Galaxies::create([
-            'name' => 'Milchstraße',
+            'name' => 'Milky Way',
         ]);
         $galaxy->save();
 
-        $planet = Planets::create([
-            'name' => 'Erde',
+        $galaxy = Galaxies::create([
+            'name' => 'Andromeda',
         ]);
-        $planet->galaxy()->associate($galaxy);
-        $planet->save();
+        $galaxy->save();
 
-        $base = Bases::create([
-            'name' => 'StupidBase',
-            'created_at' => Carbon::now(),
-            'last_upgraded_at' => Carbon::now(),
+        $galaxy = Galaxies::create([
+            'name' => 'Whirlpool Galaxy',
         ]);
-        $base->user()->associate($user);
-        $base->planet()->associate($planet);
-        $base->save();
+        $galaxy->save();
 
-        $harbour = Harbours::create([
+        $galaxy = Galaxies::create([
+            'name' => 'LMC',
         ]);
-        $harbour->base()->associate($base);
-        $harbour->save();
-
-        $fleet = Fleets::create([
-            'name' => 'Affenbande',
-        ]);
-        $fleet->harbour()->associate($harbour);
-        $fleet->save();
-
-        $resources = Resources::create([
-            'metal' => 0,
-            'gas' => 0,
-            'gems' => 0,
-        ]);
-        $resources->base()->associate($base);
-        $resources->save();
-
-        $metal_collector = ResourceCollectors::create([
-            'type' => 'metal',
-            'last_collected' => Carbon::now(),
-            'level' => 1,
-        ]);
-        $metal_collector->base()->associate($base);
-        $metal_collector->save();
-
-        $gas_collector = ResourceCollectors::create([
-            'type' => 'gas',
-            'last_collected' => Carbon::now(),
-            'level' => 1,
-        ]);
-        $gas_collector->base()->associate($base);
-        $gas_collector->save();
-
-        $gem_collector = ResourceCollectors::create([
-            'type' => 'gems',
-            'last_collected' => Carbon::now(),
-            'level' => 1,
-        ]);
-        $gem_collector->base()->associate($base);
-        $gem_collector->save();
+        $galaxy->save();
 
         $fleet = Fleets::create([
             'name' => 'Pirates_easy',
@@ -123,5 +65,6 @@ class DatabaseSeeder extends Seeder
             'busy' => false,
         ]);
         $fleet->save();
+
     }
 }
