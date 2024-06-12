e8e2b5cccdb8a8292199af625976fc30fea6a508
johannes <johannes.hermann@sandstorm.de>
2024-06-12 21:35:47 +0200

    FIX: email referral code nullable

--- laravel-backend/database/migrations/2024_05_17_140446_referral_code_user.php
+++ laravel-backend/database/migrations/2024_05_17_140446_referral_code_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('referral_code');
+            $table->string('referral_code')->nullable();
         });
     }
 
