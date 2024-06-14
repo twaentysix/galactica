b4bcb0db9293f45f9146936430142e3199e726f0
johannes <johannes.hermann@sandstorm.de>
2024-06-14 11:04:24 +0200

    TASK: add email verified at to admin panel for users

--- laravel-backend/app/Filament/Resources/UserResource.php
+++ laravel-backend/app/Filament/Resources/UserResource.php
@@ -26,6 +26,7 @@ class UserResource extends Resource
                 Forms\Components\TextInput::make('name'),
                 Forms\Components\TextInput::make('email')
                     ->email(),
+                Forms\Components\DateTimePicker::make('email_verified_at'),
             ]);
     }
 
@@ -35,6 +36,8 @@ class UserResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
                 Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                ->dateTime(),
             ])
             ->filters([
                 //
